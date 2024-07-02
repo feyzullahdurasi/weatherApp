a5c36af2e43cd3e1ecd36b81c38291d4f1624e61
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-07-02 12:11:00 +0300

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Feyzullah Durası on 2.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
